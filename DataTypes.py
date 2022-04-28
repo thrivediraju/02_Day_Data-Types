663214ba3575a7815a0a0c3827c9c06ad952986e
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-04-28 21:50:36 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,34 @@
+'''
+Date: 19th April 2022 Tuesday
+Name: Hd
+File Desc: integer , float , string and boolean
+i.e.  int . float , str , bool
+
+type() method will help us to know what datatype it is.
+'''
+print(type(2))
+print(type(0))
+print(type(-2))
+print(type(-898))
+print(type(.0))
+print(type(0.0))
+print(type(-3.4))
+print(type(6.7))
+
+"""
+strings:
+Anything enclosed between single quotes or double quotes
+or three times single quotes or three times double quotes
+kept inside the print statement or assigned to a variable
+is called as strings.
+"""
+
+# print(type('h'))
+# print(type("e"))
+# print(type('''y'''))
+# print(type("""Python"""))
+
+# print(5<8)
+# print(19<3)
+# print(type(True))
+# print(type(False))
---
+++ variable.py
@@ -0,0 +1,44 @@
+"""
+Date: 19th April 2022 Tuesday
+Name: HD
+File Desc: Rules of Variable, identifer.
+working with isidentifier()
+"""
+# print('a'.isidentifier())
+# print('_'.isidentifier())
+# print('_a'.isidentifier())
+# print('4_'.isidentifier())
+# print('w4_'.isidentifier())
+# print('n.4_'.isidentifier())
+# print('n 4_'.isidentifier())
+
+
+# a=4 ; _=6
+# print(a)
+# print(_)
+
+# _c_=8 ; x3r=5
+# print(_c_) ; print(x3r)
+
+""" ways to assign variables """
+# a=3 ; b=4
+# print(a)
+# print(b)
+# print(a) ; print(b)
+# print(a,b)
+
+x,y,z=5,7,8
+print(x)
+print(y)
+print(z)
+print(x,y,z)
+print(x,z,z)
+print(x) ; print(y); print(z)
+
+# p=q=r=6 ; a=4 ; c=8
+# print(p)
+# print(q)
+# print(r)
+# print(p,q,r)
+# print(a,p,c)
+# print(x) # NameError: name 'x' is not defined
